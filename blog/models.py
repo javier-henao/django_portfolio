4bb103e5c3c808e5ef05a35311f266d3dd23bdc3
Javier Henao <javher@MacBook-Air-de-Javier.local>
2026-01-29 20:54:49 -0500

    finalizacion blog y enlaces

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,16 @@
+import datetime
 from django.db import models
+from django.db.models.fields import CharField, TextField, DateField
+from django.db.models.fields.files import ImageField
 
 # Create your models here.
 
+
+class Post(models.Model):
+    title = CharField(max_length=100)
+    description = TextField()
+    image = ImageField(upload_to='blog/images')
+    date = DateField(datetime.date.today)
+
+    def __str__(self):
+        return self.title
