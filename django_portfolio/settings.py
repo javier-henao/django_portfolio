ef1c7ed719f00379eabcda7b42816255495cb897
Javier Henao <javier.henao@outlook.com>
2026-01-31 11:48:17 -0500

    settings.py, ALLOWED HOST

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -128,3 +128,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS=['localhost','web-production-1ee9b.up.railway.app']
